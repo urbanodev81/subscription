4839b68e8be8c332159737a0ee56fc4671bde9e4
ricardo.urbano <ricardo.urbano@cejam.org.br>
2023-07-21 16:52:34 -0300

    authenticacao instalada

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,4 @@
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,20 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+
+          {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+          {{-- <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+          <link href="{{ mix('js/app.js') }}" rel="stylesheet"> --}}
+
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+
+
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
